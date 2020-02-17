0955ef82671740287988582dad412b274e22eb9f
mikesae <msaelens@gmail.com>
2020-02-17 17:26:31 -0500

    Use map to convert graduation year to class.

--- src/player.tsx
+++ src/player.tsx
@@ -8,12 +8,20 @@ interface PlayerProps {
     year: string;
 }
 
+const YearMap = new Map([
+    ['2020', 'Sr.'],
+    ['2021', 'Jr.'],
+    ['2022', 'So.'],
+    ['2023', 'Fr.']
+]);
+
+
 export const Player = (props: PlayerProps) =>
     <div className="row">
         <div className="col-1">{props.uniformNumber}</div>
         <div className="col-7">{props.name}</div>
         <div className="col-2">{props.position}</div>
-        <div className="col-2">{props.year}</div>
+        <div className="col-2">{YearMap.get(props.year)}</div>
     </div>;
 
 export default Player;
