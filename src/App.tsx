529a6d27c788e38bd0f6b4372c5ff510a486aeef
mikesae <msaelens@gmail.com>
2020-02-17 20:06:06 -0500

    Don't enlarge font-size if android.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Roster from "./roster";
 
-function App() {
-  return (
-    <div className="App">
-        <div className="jumbotron jumbotron-fluid">
-            <h3>2019-20 Shen Varsity Hockey</h3>
-        </div>
-        <Roster></Roster>
-    </div>
-  );
+class App extends Component {
+    isAndroid() {
+        const userAgent = navigator.userAgent.toLowerCase();
+        return userAgent.indexOf('android') !== -1;
+    };
+    componentWillMount() {
+        if (this.isAndroid()) {
+            require('./android.css');
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="jumbotron jumbotron-fluid">
+                    <h3>2019-20 Shen Varsity Hockey</h3>
+                </div>
+                <Roster></Roster>
+            </div>
+        );
+    }
 }
 
 export default App;
