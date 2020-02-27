36655419943e7bdb3b8b48e6948a6813a9ff0346
msaelens <michael.saelens@healthcaresource.com>
2020-02-27 14:25:30 -0500

    Put app header inside container.

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import React, {Component} from 'react';
 import './App.scss';
 import Roster from "./roster";
 import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <Container className="App container-fluid">
                 <div className="jumbotron jumbotron-fluid">
                     <Image roundedCircle thumbnail src="maskable_icon.png"/>
                     <span className="title">2019-20 Shen Varsity Hockey</span>
                 </div>
                 <Roster/>
-            </div>
+            </Container>
         );
     }
 }
