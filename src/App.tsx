449a0f068a1930cdd89f3e11b63c656d6ca70aae
mikesae <msaelens@gmail.com>
2020-02-27 22:08:20 -0500

    Add shen hockey logo, which has a transparent background.

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ class App extends Component {
             <Container className="App container-fluid">
                 <Row>
                     <Col className="col-2 px-0 my-auto">
-                        <Image roundedCircle thumbnail src="maskable_icon.png"/>
+                        <Image thumbnail src="shen-hockey-logo.png"/>
                     </Col>
                     <Col className="col-10 px-0 my-auto">
                         <div className="title">2019-20 Shen Varsity Hockey</div>
