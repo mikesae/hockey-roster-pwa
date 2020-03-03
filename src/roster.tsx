dd9848ea54cc181356fbfa133ea58ba9ecdd58d8
msaelens <michael.saelens@healthcaresource.com>
2020-03-03 09:57:09 -0500

    Put no-bounce on top header again.

--- src/roster.tsx
+++ src/roster.tsx
@@ -5,29 +5,31 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import {IPlayerProps} from "./IPlayerProps";
 import Container from "react-bootstrap/Container";
-import './Roster.scss';
 
 export default class Roster extends Component {
     render() {
         let roster: any[] = [];
 
-        players.sort((a:IPlayerProps, b:IPlayerProps) => a.uniformNumber - b.uniformNumber);
+        players.sort((a: IPlayerProps, b: IPlayerProps) => a.uniformNumber - b.uniformNumber);
         players.forEach(player => {
             roster.push(<PlayerSummary {...player} key={player.uniformNumber}/>)
         });
 
         return (
-            <Container className="no-bounce">
-                <Row>
-                    <Col className="col-2 px-0"></Col>
-                    <Col className="col-1 px-0">#</Col>
-                    <Col className="col-5 px-0">Name</Col>
-                    <Col className="col-2 px-0">Pos.</Col>
-                    <Col className="col-1 px-0">Year</Col>
-                </Row>
-                {roster}
-                <div className="bottom-spacer"/>
-            </Container>
+            <>
+                <div className="spacer"/>
+                <Container>
+                    <Row>
+                        <Col className="col-2 px-0"></Col>
+                        <Col className="col-1 px-0">#</Col>
+                        <Col className="col-5 px-0">Name</Col>
+                        <Col className="col-2 px-0">Pos.</Col>
+                        <Col className="col-1 px-0">Year</Col>
+                    </Row>
+                    {roster}
+                </Container>
+                <div className="spacer"/>
+            </>
         );
     }
 }
