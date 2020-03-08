78e13b4ed8c552762dd99d7a70a2f4626b383c0c
mikesae <msaelens@gmail.com>
2020-03-07 22:47:40 -0500

    Add bio to player detail.

--- src/PlayerDetail.tsx
+++ src/PlayerDetail.tsx
@@ -3,7 +3,6 @@ import './PlayerDetail.scss';
 import players from './players.json';
 import Container from "react-bootstrap/Container";
 import Image from 'react-bootstrap/Image';
-import FormLabel from "react-bootstrap/FormLabel";
 import FormGroup from "react-bootstrap/esm/FormGroup";
 import TopNavbar from "./TopNavbar";
 import BottomNavbar from "./BottomNavbar";
@@ -21,20 +20,48 @@ export class PlayerDetail extends Component {
 
         return (
             <>
-                <TopNavbar title={player.name} showBackNav={true}/>
-                <Container className="container">
+                <TopNavbar title={`#${player.uniformNumber}  ${player.name}`} showBackNav={true}/>
+                <Container className="container container-player-detail">
                     <FormGroup>
                         <Image roundedCircle src={player.imageUrl} alt={player.name}/>
                     </FormGroup>
                     <FormGroup>
-                        <div>
-                            <FormLabel>Graduation Year: </FormLabel>
-                            <span>{player.year}</span>
-                        </div>
-                        <div>
-                            <FormLabel>Position: </FormLabel>
-                            <span>{player.position}</span>
-                        </div>
+                        {player.pumpUpSong &&
+                            <div>
+                                <div className="player-form-label">Pump-Up Song</div>
+                                <div><q>{player.pumpUpSong}</q> by {player.pumpUpSongArtist}</div>
+                            </div>
+                        }
+                        {player.favoriteFood &&
+                            <div>
+                                <div className="player-form-label">Favorite Food</div>
+                                <div>{player.favoriteFood}</div>
+                            </div>
+                        }
+                        { player.favorteTVShow &&
+                            <div>
+                                <div className="player-form-label">Favorite TV Show/Movie</div>
+                                <div>{player.favoriteTVShow}</div>
+                            </div>
+                        }
+                        {player.spiritAnimal &&
+                            <div>
+                                <div className="player-form-label">Spirit Animal</div>
+                                <div>{player.spiritAnimal}</div>
+                            </div>
+                        }
+                        {player.wordsOfWisdom &&
+                            <div>
+                                <div className="player-form-label">Words of Wisdom</div>
+                                <div><q>{player.wordsOfWisdom}</q> - <i>{player.wordsOfWisdomAttribute}</i></div>
+                            </div>
+                        }
+                        {player.wordsOfGratitude &&
+                            <div>
+                                <div className="player-form-label">Words of Gratitude</div>
+                                <div>{player.wordsOfGratitude}</div>
+                            </div>
+                        }
                     </FormGroup>
                 </Container>
                 <BottomNavbar/>
