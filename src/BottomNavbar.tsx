4bd99f090779986ef2cb3221d4ef609560a4f26e
mikes <msaelens@gmail.com>
2020-03-14 09:41:47 -0400

    Remove buttons around bottom navbar links to fix navigation issue.

--- src/BottomNavbar.tsx
+++ src/BottomNavbar.tsx
@@ -1,12 +1,11 @@
-import React, {Component} from "react";
-import Navbar from "react-bootstrap/Navbar";
-import NavItem from "react-bootstrap/NavItem";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faThList} from "@fortawesome/free-solid-svg-icons/faThList";
-import {faCalendarAlt} from "@fortawesome/free-solid-svg-icons";
-import { NavLink } from "react-router-dom";
-import "./BottomNavbar.scss";
-import Button from "react-bootstrap/Button";
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import NavItem from 'react-bootstrap/NavItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThList } from '@fortawesome/free-solid-svg-icons/faThList';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from 'react-router-dom';
+import './BottomNavbar.scss';
 
 export default class BottomNavbar extends Component {
     render() {
@@ -14,37 +13,35 @@ export default class BottomNavbar extends Component {
             <>
                 <div className="spacer"/>
                 <Navbar className="jumbotron-fluid border-top justify-content-center navbar-bottom" fixed="bottom">
-                <NavItem className="text-center">
-                    <NavLink
-                        to="/"
-                        isActive={(match, location) => {
-                            if (!match) {
-                                return false;
-                            }
-                            return match.isExact;
-                        }}
-                    >
-                        <Button>
+                    <NavItem className="text-center">
+                        <NavLink
+                            to="/"
+                            isActive={(match, location) => {
+                                if (!match) {
+                                    return false;
+                                }
+                                return match.isExact;
+                            }}
+                        >
                             <FontAwesomeIcon className="link-icon" icon={faThList}/>
                             <div className="icon-label">Roster</div>
-                        </Button>
-                    </NavLink>
-                </NavItem>
-                <NavItem className="text-center">
-                    <NavLink
-                        to="/schedule"
-                        isActive={(match, location) => {
-                            if (!match) {
-                                return false;
-                            }
-                            return match.isExact;
-                        }}
-                     >
-                        <FontAwesomeIcon className="link-icon" icon={faCalendarAlt}/>
-                        <div className="icon-label">Schedule</div>
-                    </NavLink>
-                </NavItem>
-            </Navbar>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem className="text-center">
+                        <NavLink
+                            to="/schedule"
+                            isActive={(match, location) => {
+                                if (!match) {
+                                    return false;
+                                }
+                                return match.isExact;
+                            }}
+                        >
+                            <FontAwesomeIcon className="link-icon" icon={faCalendarAlt}/>
+                            <div className="icon-label">Schedule</div>
+                        </NavLink>
+                    </NavItem>
+                </Navbar>
             </>
         );
     }
