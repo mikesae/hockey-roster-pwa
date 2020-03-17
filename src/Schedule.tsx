30064db71d4658e614c3eca4c627530a3043737b
mikesae <msaelens@gmail.com>
2020-03-17 07:26:48 -0400

    Make navigation bars and their text larger.

--- src/Schedule.tsx
+++ src/Schedule.tsx
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 import Container from "react-bootstrap/Container";
 import TopNavbar from "./TopNavbar";
 import BottomNavbar from "./BottomNavbar";
+import './Schedule.scss';
 
 export class Schedule extends Component {
     render() {
         return (
             <>
                 <TopNavbar title="Schedule" showBackNav={true}/>
-                <Container className="App container-fluid">
+                <Container className="container">
                     <h4>TBD</h4>
                 </Container>
                 <BottomNavbar/>
