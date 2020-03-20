d78fce49c40c576b8394aea939bbf71db61ea2f4
mikes <msaelens@gmail.com>
2020-03-20 07:27:31 -0400

    Add slide animation between routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.scss';
-import Roster from './roster';
-import {Route, Switch} from 'react-router-dom';
-import {PlayerDetail} from "./PlayerDetail";
-import {Schedule} from "./Schedule";
-import {BrowserRouter} from "react-router-dom";
-import Credits from './credits';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
+import BottomNavbar from './BottomNavbar';
 
 export default class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Switch>
-                    <Route exact path="/" component={Roster}/>
-                    <Route path="/player/:number" component={PlayerDetail}/>
-                    <Route path="/credits" component={Credits}/>
-                    <Route exact path="/schedule" component={Schedule}/>
-                </Switch>
+                <Routes/>
+                <BottomNavbar/>
             </BrowserRouter>
         );
     }
