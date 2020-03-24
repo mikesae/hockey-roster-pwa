d251b916f505264f8cc8b1604ade16ec99c54a37
msaelens <michael.saelens@healthcaresource.com>
2020-03-24 14:41:09 -0400

    Change navbar bottom icons based on which route is active.

--- src/BottomNavbar.tsx
+++ src/BottomNavbar.tsx
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import NavItem from 'react-bootstrap/NavItem';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
-import { NavLink } from 'react-router-dom';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCalendar, faCalendarAlt, faUser, faVideo} from '@fortawesome/free-solid-svg-icons';
+import {NavLink} from 'react-router-dom';
 import './BottomNavbar.scss';
-import { faFilm } from '@fortawesome/free-solid-svg-icons/faFilm';
-import { faUsers } from '@fortawesome/free-solid-svg-icons/faUsers';
+import {faFilm} from '@fortawesome/free-solid-svg-icons/faFilm';
+import {faUsers} from '@fortawesome/free-solid-svg-icons/faUsers';
 
-export default class BottomNavbar extends Component {
+export default class BottomNavbar extends Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {activeLink: '/'};
+    }
+
+    changeActiveLink(linkName: string) {
+        if (this.state.activeLink !== linkName) {
+            this.setState({activeLink: linkName});
+        }
+    }
+
     render() {
+        const activeLink = this.state.activeLink;
+
         return (
             <Navbar className="jumbotron-fluid border-top justify-content-center navbar-bottom" fixed="bottom">
                 <NavItem className="text-center">
@@ -19,12 +32,18 @@ export default class BottomNavbar extends Component {
                             if (!match) {
                                 return false;
                             }
+                            if (match.isExact) {
+                                this.changeActiveLink('/');
+                            }
                             return match.isExact;
                         }}
                         activeClassName="active"
                     >
                         <div className="btn">
-                            <FontAwesomeIcon icon={faUsers} className="link-icon"/>
+                            <FontAwesomeIcon
+                                className="link-icon"
+                                icon={activeLink === '/' ? faUsers : faUser}
+                            />
                             <div className="icon-label">Roster</div>
                         </div>
                     </NavLink>
@@ -36,29 +55,41 @@ export default class BottomNavbar extends Component {
                             if (!match) {
                                 return false;
                             }
+                            if (match.isExact) {
+                                this.changeActiveLink('schedule');
+                            }
                             return match.isExact;
                         }}
                         activeClassName="active"
                     >
                         <div className="btn">
-                            <FontAwesomeIcon className="link-icon" icon={faCalendarAlt}/>
+                            <FontAwesomeIcon
+                                className="link-icon"
+                                icon={activeLink === 'schedule' ? faCalendarAlt : faCalendar}
+                            />
                             <div className="icon-label">Schedule</div>
                         </div>
                     </NavLink>
                 </NavItem>
                 <NavItem className="text-center">
                     <NavLink
-                         to="/credits"
-                         isActive={match => {
-                             if (!match) {
-                                 return false;
-                             }
-                             return match.isExact;
-                         }}
-                         activeClassName="active"
+                        to="/credits"
+                        isActive={match => {
+                            if (!match) {
+                                return false;
+                            }
+                            if (match.isExact) {
+                                this.changeActiveLink('credits');
+                            }
+                            return match.isExact;
+                        }}
+                        activeClassName="active"
                     >
                         <div className="btn">
-                            <FontAwesomeIcon className="link-icon" icon={faFilm}/>
+                            <FontAwesomeIcon
+                                className="link-icon"
+                                icon={activeLink === 'credits' ? faFilm : faVideo}
+                            />
                             <div className="icon-label">Credits</div>
                         </div>
                     </NavLink>
