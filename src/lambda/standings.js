557ccb77694ba7ca581e0b11103d54a96da970ca
mikesae <msaelens@gmail.com>
2020-04-11 08:57:26 -0400

    Add standings endpoint.

--- src/lambda/standings.js
+++ src/lambda/standings.js
@@ -1,15 +1,35 @@
 const tabletojson = require('tabletojson').Tabletojson;
-const standingsUrl = 'https://www.section2hockey.com/teams/default.asp?u=CDHSHL&s=hockey'
+const standingsUrl = 'https://www.section2hockey.com/teams/default.asp?u=CDHSHL&s=hockey';
 
 exports.handler = async event => {
     const season = event.queryStringParameters.season;
-    await tabletojson.convertUrl(
-        `${standingsUrl}&p=standings&viewseas=${season}`,
-        (tablesAsJson) => {
-            return {
-                statusCode: 200,
-                body: tablesAsJson[1]
-            }
-        }
-    );
-}
+    return await new Promise(resolve => {
+        tabletojson.convertUrl(
+            `${standingsUrl}&p=standings&viewseas=${season}`,
+            tablesAsJson => {
+                const rows = tablesAsJson[8].slice(3);
+                let result = [];
+                rows.forEach(row => {
+                    result.push(
+                    {
+                        team: row["0"],
+                        conferenceGamesPlayed: row["1"],
+                        conferenceRecord: row["2"],
+                        conferencePoints: row["3"],
+                        overallGamesPlayed: row["4"],
+                        overallRecord: row["5"],
+                        goalsFor: row["6"],
+                        goalsAgainst: row["7"],
+                        lastSix: row["8"],
+                        streak: row["9"],
+                        winPercentage: row["10"]
+                    });
+                });
+
+                resolve({
+                    statusCode: 200,
+                    body: JSON.stringify(result)
+                })
+            });
+    });
+};
