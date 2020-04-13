dca63bab4451dcf53b10dfbf01b2983a754a5b27
mikesae <msaelens@gmail.com>
2020-04-12 23:05:41 -0400

    Set row properties based on table headers.

--- src/lambda/playerStatistics.js
+++ src/lambda/playerStatistics.js
@@ -8,22 +8,23 @@ exports.handler = async event => {
     return await new Promise(resolve => {
         tabletojson.convertUrl(
             `${url}&viewseas=${season}&playerId=${playerId}`,
+            { useFirstRowForHeadings: true },
             tablesAsJson => {
                 const rows = tablesAsJson[9].slice(1);
                 let result = [];
                 rows.forEach(row => {
                     result.push(
                     {
-                        GameDate: row["0"],
-                        Opponent: row["2"],
-                        Result: parseInt(row["3"]),
-                        Goals: parseInt(row["5"]),
-                        Assists: parseInt(row["6"]),
-                        Points: parseInt(row["7"]),
-                        PPG: parseInt(row["8"]),
-                        GWG: parseInt(row["9"]),
-                        GTG: parseInt(row["10"]),
-                        PlusMinus: parseInt(row["11"])
+                        GameDate: row["Date"],
+                        Opponent: row["Opponent"],
+                        Result: row["Result"],
+                        Goals: parseInt(row["G"]),
+                        Assists: parseInt(row["A"]),
+                        Points: parseInt(row["Pts"]),
+                        PPG: parseInt(row["PPG"]),
+                        GWG: parseInt(row["GWG"]),
+                        GTG: parseInt(row["GTG"]),
+                        PlusMinus: parseInt(row["+/-"])
                     });
                 });
 
