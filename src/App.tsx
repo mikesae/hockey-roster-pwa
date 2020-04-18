20600b58b250824ee990593c7e8374bda4afb99f
mikes <msaelens@gmail.com>
2020-04-18 08:15:54 -0400

    Replace ease in/out transition between pages with cross-fade.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import './App.scss';
-import { BrowserRouter } from 'react-router-dom';
-import Routes from './Routes';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import BottomNavbar from './BottomNavbar';
+import ReactCSSTransitionReplace from 'react-css-transition-replace';
+import Roster from './roster';
+import Credits from './credits';
+import Schedule from './Schedule';
+import Standings from './Standings';
+import { PlayerDetail } from './PlayerDetail';
+import PlayerStatSheet from './PlayerStatSheet';
+import GoalieStatSheet from './GoalieStatSheet';
+import './transitions.scss';
 
 export default class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Routes/>
+                <Route
+                    render={({location}) => (
+                        <ReactCSSTransitionReplace
+                            transitionName="cross-fade"
+                            transitionEnterTimeout={800}
+                            transitionLeaveTimeout={800}
+                        >
+                            <div key={location.pathname}>
+                                <Switch location={location}>
+                                    <Route path="/" exact component={Roster}/>
+                                    <Route path="/Credits" component={Credits}/>
+                                    <Route path="/Schedule" component={Schedule}/>
+                                    <Route path="/Standings" component={Standings}/>
+                                    <Route path="/player/:number" component={PlayerDetail}/>
+                                    <Route path="/player-stat-sheet/:playerId" component={PlayerStatSheet}/>
+                                    <Route path="/goalie-stat-sheet/:playerId" component={GoalieStatSheet}/>
+                                </Switch>
+                            </div>
+                        </ReactCSSTransitionReplace>
+                    )}
+                />
                 <BottomNavbar/>
             </BrowserRouter>
         );
