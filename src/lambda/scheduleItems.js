db73b9d882378682dde7c5ddaebd641984fd191f
Mike S <Michael.Saelens@healthcaresource.com>
2021-02-09 17:59:11 -0500

    Add new schedule items.

--- src/lambda/scheduleItems.js
+++ src/lambda/scheduleItems.js
@@ -14,7 +14,8 @@ exports.handler = async () => {
                     "section": "II",
                     "result": "",
                     "location": "Clifton Park Arena",
-                    "locationUrl": "https://maps.apple.com/?q=Capital+Arena"
+                    "locationUrl": "https://maps.apple.com/?q=Capital+Arena",
+                    "liveFeedUrl:": "https://www.livebarn.com/en/videon?mode=live&sid=409"
                 },
                 {
                     "day": "Sat",
@@ -27,7 +28,64 @@ exports.handler = async () => {
                     "section": "II",
                     "result": "",
                     "location": "Schenectady County Rec Facility",
-                    "locationUrl": "https://maps.apple.com/?q=SCRF"
+                    "locationUrl": "https://maps.apple.com/?q=SCRF",
+                    "liveFeedUrl:": "https://www.livebarn.com/en/videon/?mode=live&sid=354"
+                },
+                {
+                    "day": "Tue",
+                    "date": "2/16/21",
+                    "time": "3:45 PM",
+                    "opponent": "Saratoga",
+                    "logoUrl": "/images/team-logos/saratoga.png",
+                    "type": "Game",
+                    "home": true,
+                    "section": "II",
+                    "result": "",
+                    "location": "Clifton Park Arena",
+                    "locationUrl": "https://maps.apple.com/?q=Capital+Arena",
+                    "liveFeedUrl:": "https://www.livebarn.com/en/videon?mode=live&sid=409"
+                },
+                {
+                    "day": "Wed",
+                    "date": "2/17/21",
+                    "time": "8?30 PM",
+                    "opponent": "Saratoga",
+                    "logoUrl": "/images/team-logos/saratoga.png",
+                    "type": "Away",
+                    "home": false,
+                    "section": "II",
+                    "result": "",
+                    "location": "Weibel Ave. Rink",
+                    "locationUrl": "https://maps.apple.com/?q=Weibel+Ave+Rink",
+                    "liveFeedUrl:": ""
+                },
+                {
+                    "day": "Fri",
+                    "date": "2/19/21",
+                    "time": "3:45 PM",
+                    "opponent": "Mohawks",
+                    "logoUrl": "/images/team-logos/mohawks.png",
+                    "type": "Game",
+                    "home": true,
+                    "section": "II",
+                    "result": "",
+                    "location": "Clifton Park Arena",
+                    "locationUrl": "https://maps.apple.com/?q=Capital+Arena",
+                    "liveFeedUrl:": "https://www.livebarn.com/en/videon?mode=live&sid=409"
+                },
+                {
+                    "day": "Sat",
+                    "date": "2/20/21",
+                    "time": "3:45 PM",
+                    "opponent": "Mohawks",
+                    "logoUrl": "/images/team-logos/mohawks.png",
+                    "type": "Away",
+                    "home": false,
+                    "section": "II",
+                    "result": "",
+                    "location": "Schenectady County Rec Facility",
+                    "locationUrl": "https://maps.apple.com/?q=SCRF",
+                    "liveFeedUrl:": "https://www.livebarn.com/en/videon/?mode=live&sid=354"
                 }
             ]
         )
