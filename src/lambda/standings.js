f528de75e682014ac8eafd30d461504ecc2c95d4
mikesae <msaelens@gmail.com>
2021-02-13 18:39:37 -0500

    Fix standings service due to change in section II standings table.

--- src/lambda/standings.js
+++ src/lambda/standings.js
@@ -1,6 +1,8 @@
 const tabletojson = require('tabletojson').Tabletojson;
 const standingsUrl = 'https://www.section2hockey.com/teams/default.asp?u=CDHSHL&s=hockey';
 
+//https://www.section2hockey.com/teams/default.asp?u=CDHSHL&s=hockey&p=standings&viewseas=Winter_2020/2021
+
 exports.handler = async event => {
     const season = event.queryStringParameters.season || 'Winter_2020%2F2021';
 
@@ -14,20 +16,22 @@ exports.handler = async event => {
                 const rows = tablesAsJson[8].slice(3);
                 let result = [];
                 rows.forEach(row => {
-                    result.push(
-                    {
-                        team: row["0"],
-                        conferenceGamesPlayed: row["1"],
-                        conferenceRecord: row["2"],
-                        conferencePoints: row["3"],
-                        overallGamesPlayed: row["4"],
-                        overallRecord: row["5"],
-                        goalsFor: row["6"],
-                        goalsAgainst: row["7"],
-                        lastSix: row["8"],
-                        streak: streak(row["9"]),
-                        winPercentage: row["10"]
-                    });
+                    if (Object.keys(row).length >= 11) {
+                        result.push(
+                            {
+                                team: row["0"],
+                                conferenceGamesPlayed: row["1"],
+                                conferenceRecord: row["2"],
+                                conferencePoints: row["3"],
+                                overallGamesPlayed: row["4"],
+                                overallRecord: row["5"],
+                                goalsFor: row["6"],
+                                goalsAgainst: row["7"],
+                                lastSix: row["8"],
+                                streak: streak(row["9"]),
+                                winPercentage: row["10"]
+                            });
+                    }
                 });
 
                 resolve({
